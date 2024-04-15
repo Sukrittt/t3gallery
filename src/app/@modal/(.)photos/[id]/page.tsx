c107a9aae772c301de37f0dd3a1b378e6b4711ea
sukrittt <sukritsaha27@gmail.com>
2024-04-16 00:27:26 +0530

    rendering images through parallel routes

--- src/app/@modal/(.)photos/[id]/page.tsx
+++ src/app/@modal/(.)photos/[id]/page.tsx
@@ -1,9 +1,31 @@
-import { Modal } from "./modal";
+import Image from "next/image";
 
-export default function PhotoModal({
+import { Modal } from "./modal";
+import { getImage } from "~/server/queries";
+
+export default async function PhotoModal({
   params: { id: photoId },
 }: {
   params: { id: string };
 }) {
-  return <Modal>{photoId}</Modal>;
+  const idAsNumber = Number(photoId);
+
+  if (isNaN(idAsNumber)) {
+    return <Modal>Invalid photo ID</Modal>;
+  }
+
+  const image = await getImage(idAsNumber);
+
+  return (
+    <Modal>
+      <div className="relative h-48 w-48 p-4">
+        <Image
+          src={image.url}
+          alt={image.name}
+          className="h-auto w-full"
+          fill
+        />
+      </div>
+    </Modal>
+  );
 }
--- src/app/photos/[id]/page.tsx
+++ src/app/photos/[id]/page.tsx
@@ -1,7 +1,31 @@
-export default function PhotoPage({
-  params: { id },
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+import { getImage } from "~/server/queries";
+
+export default async function PhotoPage({
+  params: { id: photoId },
 }: {
   params: { id: string };
 }) {
-  return <div>{id}</div>;
+  const idAsNumber = Number(photoId);
+
+  if (isNaN(idAsNumber)) {
+    notFound();
+  }
+
+  const image = await getImage(idAsNumber);
+
+  return (
+    <div>
+      <div className="relative h-48 w-48 p-4">
+        <Image
+          src={image.url}
+          alt={image.name}
+          className="h-auto w-full"
+          fill
+        />
+      </div>
+    </div>
+  );
 }
