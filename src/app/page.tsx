5a2270f358c121d507bf94deba6f445383f24cc5
sukrittt <sukritsaha27@gmail.com>
2024-04-15 20:56:45 +0530

    user auth conditional logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
 import Image from "next/image";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
+
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
+  return (
+    <div>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          <p>Please sign in.</p>
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
+    </div>
+  );
+}
+
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.createdAt),
   });
