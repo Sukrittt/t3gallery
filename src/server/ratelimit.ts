29232267f4a6e986ca0c285d3e87aecbc3ade9e2
sukrittt <sukritsaha27@gmail.com>
2024-04-16 23:46:28 +0530

    ratelimiting file uploads

---
+++ src/server/ratelimit.ts
@@ -0,0 +1,10 @@
+import { Redis } from "@upstash/redis";
+import { Ratelimit } from "@upstash/ratelimit";
+
+// Create a new ratelimiter, that allows 10 requests per 10 seconds
+export const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(10, "10 s"),
+  analytics: true,
+  prefix: "@upstash/ratelimit",
+});
